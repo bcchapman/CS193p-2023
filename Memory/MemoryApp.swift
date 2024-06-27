5a6c39c474d5d4a12803f08e86c972b50e638f68
Bryan Chapman <bryan.chapmanbb@gmail.com>
2024-06-26 21:18:30 -0700

    Initial Commit

---
+++ Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Bryan Chapman on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
